ef278ed55f75a14faeb175f6d64f6816c9f1ae9f
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-02-18 11:56:42 -0800

    Replaced placeholder code with beginning array of 3 pokemon objects
    Each pokemon will have the keys of name, height, types, and weight

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello World!!');
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 2,
+    types: ['grass', 'poison'],
+    weight: '15.2 lbs',
+  },
+  {
+    name: 'Ivysaur',
+    height: 3,
+    types: ['grass', 'fighting'],
+    weight: '15.2 lbs',
+  },
+  {
+    name: 'Venasaur',
+    height: 6,
+    types: ['grass', 'fighting'],
+    weight: '15.2 lbs',
+  },
+];
