19520f2e90d50ad8747f070b9a95b8058b1219c8
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-02-19 16:33:59 -0800

    Added a for loop to Print pokemon name and height
    If the pokemon has a height of more than 5, it will also print 'That's one tall Pokemon!' to the DOM as well.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Array of pokemon, listing characteristics such as height and type//
 let pokemonList = [
   {
     name: 'Bulbasaur',
@@ -18,3 +19,22 @@ let pokemonList = [
     weight: '15.2 lbs',
   },
 ];
+
+/*This loop will iterate through the pokemon,
+checking to see if the pokemon has a height
+greater than 5, if so it will print something
+different than the rest of the pokemon with
+a height less than 5*/
+for (i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5) {
+    document.write(
+      `${pokemonList[i].name} <br> Height: ${pokemonList[i].height} --- That's one tall Pokemon! <br>`
+    );
+    document.write('<br');
+  } else {
+    document.write(
+      `${pokemonList[i].name} <br> Height: ${pokemonList[i].height} <br>`
+    );
+    document.write('<br>');
+  }
+}
