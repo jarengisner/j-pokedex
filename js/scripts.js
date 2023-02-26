6c7ee79da1e9bb2646d53db9f65d855abca7a211
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-02-26 14:23:29 -0800

    Refactored for loop to forEach to iterate over the pokemon
    Instead of using a for loop to iterate over the pokemon in the list, we instead used a forEach loop to iterate over them and print the same details.

--- js/scripts.js
+++ js/scripts.js
@@ -20,21 +20,16 @@ let pokemonList = [
   },
 ];
 
-/*This loop will iterate through the pokemon,
-checking to see if the pokemon has a height
-greater than 5, if so it will print something
-different than the rest of the pokemon with
-a height less than 5*/
-for (i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 5) {
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height > 5) {
     document.write(
-      `${pokemonList[i].name} <br> Height: ${pokemonList[i].height} --- That's one tall Pokemon! <br>`
+      `<p>${pokemon.name}</p> <p>Height: ${pokemon.height} --- That's one tall Pokemon! </p>`
     );
-    document.write('<br');
+    document.write('<br>');
   } else {
     document.write(
-      `${pokemonList[i].name} <br> Height: ${pokemonList[i].height} <br>`
+      `<p>${pokemon.name} </p> <p> Height: ${pokemon.height} </p>`
     );
     document.write('<br>');
   }
-}
+});
