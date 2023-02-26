38af9c295b6ff25d9df5e1ca7a0527b0e7b1411f
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-02-26 14:56:49 -0800

    Rewrote old pokemon program as IIDE
    Added pokemon list inside an IIDE that returns an object with the ability to add pokemon to the list, and also to get all the pokemon to the list. Adjusted forEach to go function with new changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,50 @@
-//Array of pokemon, listing characteristics such as height and type//
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 2,
-    types: ['grass', 'poison'],
-    weight: '15.2 lbs',
-  },
-  {
-    name: 'Ivysaur',
-    height: 3,
-    types: ['grass', 'fighting'],
-    weight: '15.2 lbs',
-  },
-  {
-    name: 'Venasaur',
-    height: 6,
-    types: ['grass', 'fighting'],
-    weight: '15.2 lbs',
-  },
-];
+//IIDE to wrap the pokemon objects, containing their characteristics//
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 2,
+      types: ['grass', 'poison'],
+      weight: '15.2 lbs',
+    },
+    {
+      name: 'Ivysaur',
+      height: 3,
+      types: ['grass', 'fighting'],
+      weight: '15.2 lbs',
+    },
+    {
+      name: 'Venasaur',
+      height: 6,
+      types: ['grass', 'fighting'],
+      weight: '15.2 lbs',
+    },
+  ];
+  //function used to add pokemon to the pokemonList//
+  function add(name, height, [types], weight) {
+    return pokemonList.push({
+      name: name,
+      height: height,
+      types: [types],
+      weight: weight,
+    });
+  }
+  //function gets all pokemon in the pokemonList//
+  function getPokemon() {
+    return pokemonList;
+  }
+  //function returns an object with keys associated to the above functions//
+  return {
+    add: add,
+    getPokemon: getPokemon,
+  };
+})();
 
-pokemonList.forEach(function (pokemon) {
+//call to test the add functionality//
+pokemonRepository.add('Charmander', 3, ['fire', 'fighting', 2.5]);
+
+/*for each loop that prints detials for each pokemon in pokemonList, accessed through the object returned by pokemonRepository*/
+pokemonRepository.getPokemon().forEach(function (pokemon) {
   if (pokemon.height > 5) {
     document.write(
       `<p>${pokemon.name}</p> <p>Height: ${pokemon.height} --- That's one tall Pokemon! </p>`
