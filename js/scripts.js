17b1b358bc1f50586c50f545a1e6b6f85d0381f7
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-03-03 19:40:19 -0800

    Created event listener, waiting for a click to display all pokemon details

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ let pokemonRepository = (function () {
       weight: '15.2 lbs',
     },
   ];
+
   //function used to add pokemon to the pokemonList//
   function add(name, height, [...types], weight) {
     return pokemonList.push({
@@ -29,19 +30,31 @@ let pokemonRepository = (function () {
       weight: weight,
     });
   }
+
   //function gets all pokemon in the pokemonList//
   function getPokemon() {
     return pokemonList;
   }
+
+  //Function that adds an item to the list, then creates a button li and appends it to the DOM//
   function addListItem(pokemon) {
+    //selectors//
     let pokemonUnorderedList = document.querySelector('.pokemon-list');
     let pokemonListItem = document.createElement('li');
     let pokemonButton = document.createElement('button');
+    //changes inner text, gives buttons a class, and appends them to the DOM//
     pokemonButton.innerText = pokemon.name;
     pokemonButton.classList.add('poke-box');
     pokemonListItem.appendChild(pokemonButton);
     pokemonUnorderedList.appendChild(pokemonListItem);
+    //event listener for showing more details//
+    pokemonButton.addEventListener('click', showDetails);
   }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   //function returns an object with keys associated to the above functions//
   return {
     add: add,
@@ -51,7 +64,7 @@ let pokemonRepository = (function () {
 })();
 
 //call to test the add functionality//
-pokemonRepository.add('Charmander', 3, ['fire', 'fighting'], 2.5);
+//pokemonRepository.add('Charmander', 3, ['fire', 'fighting'], 2.5);
 
 /*for each loop that prints detials for each pokemon in pokemonList, accessed through the object returned by pokemonRepository*/
 pokemonRepository.getPokemon().forEach(function (pokemon) {
