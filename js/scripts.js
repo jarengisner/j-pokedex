806494f7782388fb751bb1ffb5630c3d8d87819b
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-03-07 18:57:50 -0800

    Made changes to script to fetch pokemon from the api
    Made changes to script to fetch pokemon, from the pokemon api, and display them on my page, instead of static data. On click, the details of the pokemon are fetched from their details url in the api.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
 //IIDE to wrap the pokemon objects, containing their characteristics//
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 2,
-      weight: '15.2 lbs',
-    },
-    {
-      name: 'Ivysaur',
-      height: 3,
-      weight: '15.2 lbs',
-    },
-    {
-      name: 'Venasaur',
-      height: 6,
-      weight: '15.2 lbs',
-    },
-  ];
-
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000';
   //function used to add pokemon to the pokemonList//
-  function add(name, height, weight) {
-    return pokemonList.push({
-      name: name,
-      height: height,
-      weight: weight,
-    });
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
 
   //function gets all pokemon in the pokemonList//
@@ -44,14 +24,54 @@ let pokemonRepository = (function () {
     pokemonListItem.appendChild(pokemonButton);
     pokemonUnorderedList.appendChild(pokemonListItem);
     //event listener for showing more details//
-    //Remember that the callback function always accepts event as its parameter, don't try to add the function here just alone like before//
-    pokemonButton.addEventListener('click', function () {
+    //callback function always accepts event as its parameter, don't try to add the function here just alone like before//
+    pokemonButton.addEventListener('click', function (event) {
       showDetails(pokemon);
     });
   }
 
-  function showDetails(pokemon) {
-    console.log(pokemon);
+  //Beginning of fetch function to operate with the API//
+  //Loads the list of pokemon from the pokemonAPI//
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          //Adds the pokemon generated above from API to pokemonList//
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+  //loads details by fetching from individual pokemon's details url//
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+  //function calls loadDetails on individual pokemon, then logs it out, connected to event listener//
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
   }
 
   //function returns an object with keys associated to the above functions//
@@ -59,13 +79,15 @@ let pokemonRepository = (function () {
     add: add,
     getPokemon: getPokemon,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
   };
 })();
-
-//call to test the add functionality//
-pokemonRepository.add('Charmander', 3, 2.5);
-
-/*for each loop that prints detials for each pokemon in pokemonList, accessed through the object returned by pokemonRepository*/
-pokemonRepository.getPokemon().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+//loads the list of pokemon from the API, then gets all those pokemon, and creates individual buttons for each//
+//Updated forEach from before//
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getPokemon().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
