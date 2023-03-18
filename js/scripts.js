02a8082865ae03cb086593850764837c3bda4965
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-03-17 20:43:44 -0700

    Added modal, list styling, and navbar from bootstrap
    After adding the elements from bootstrap, I deleted the unused CSS, JS, and HTML elements.

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,8 @@ let pokemonRepository = (function () {
     //changes inner text, gives buttons a class, and appends them to the DOM//
     pokemonButton.innerText = pokemon.name;
     pokemonButton.classList.add('btn-dark', 'btn-block', 'btn-primary');
+    pokemonButton.setAttribute('data-toggle', 'modal');
+    pokemonButton.setAttribute('data-target', '#exampleModal');
     pokemonListItem.appendChild(pokemonButton);
     listDiv.appendChild(pokemonListItem);
     //event listener for showing more details//
@@ -72,6 +74,7 @@ let pokemonRepository = (function () {
         hideLoadScreen();
       });
   }
+
   //loads details by fetching from individual pokemon's details url//
   function loadDetails(item) {
     showLoadScreen();
@@ -84,6 +87,7 @@ let pokemonRepository = (function () {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
         item.types = details.types;
+        item.weight = details.weight;
         hideLoadScreen();
       })
       .catch(function (e) {
@@ -91,53 +95,44 @@ let pokemonRepository = (function () {
         hideLoadScreen();
       });
   }
+
   //function calls loadDetails on individual pokemon, then logs it out, connected to event listener//
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       showModal(item);
     });
   }
+
   //space for the showModal function//
   //instead of showdetails calling console log it will call this//
   function showModal(item) {
-    modalContainer.innerHTML = '';
+    let modalName = document.querySelector('.modal-title');
+    let modalMain = document.querySelector('.modal-body');
+    let modalFoot = document.querySelector('.modal-footer');
 
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
+    modalName.innerText = '';
+    modalMain.innerHTML = '';
 
+    modalName.innerText = item.name;
     let sprite = document.createElement('img');
     sprite.setAttribute('src', item.imageUrl);
     sprite.classList.add('poke-sprite');
 
-    let modalTitle = document.createElement('h1');
-    modalTitle.classList.add('modal-title');
-    modalTitle.innerText = item.name;
+    let modalHeight = document.createElement('p');
+    modalHeight.classList.add('.modal-text');
+    modalHeight.innerText = `Height: ${item.height}`;
 
-    let modalText = document.createElement('p');
-    modalText.classList.add('.modal-text');
-    modalText.innerText = `Height: ${item.height}`;
+    let modalWeight = document.createElement('p');
+    modalWeight.classList.add('.modal-text');
+    modalWeight.innerText = `Weight: ${item.weight}`;
 
     let modalTypesContainer = document.createElement('ul');
     modalTypesContainer.classList.add('.types-container');
 
-    let modalCloseButton = document.createElement('button');
-    modalCloseButton.classList.add('close-button');
-    modalCloseButton.innerText = 'Close';
-    modalCloseButton.addEventListener('click', () => {
-      hideModal();
-    });
-    //determines what type the pokemon is and then displays that as a button for the type//
     item.types.forEach((type) => {
       typeCreator(type);
     });
 
-    modal.appendChild(modalCloseButton);
-    modal.appendChild(sprite);
-    modal.appendChild(modalTitle);
-    modal.appendChild(modalText);
-    modal.appendChild(modalTypesContainer);
-    modalContainer.appendChild(modal);
-    //creates the button for the type//
     function typeCreator(type) {
       let typesListItem = document.createElement('li');
       typesListItem.classList.add('typeLi');
@@ -147,32 +142,18 @@ let pokemonRepository = (function () {
       typesListItem.appendChild(typeButton);
       modalTypesContainer.appendChild(typesListItem);
       typeColor(typeButton.innerText);
-      //determines the styling for the type button, based off of their type//
+
       function typeColor(text) {
         typeButton.classList.add(`${text}-type`);
       }
-
-      //add classlist here, and point it to conditional function for certain type and associate it with color//
     }
-    modalContainer.classList.add('is-visible');
+    modalMain.appendChild(sprite);
+    modalMain.appendChild(modalHeight);
+    modalMain.appendChild(modalWeight);
+    modalMain.appendChild(modalTypesContainer);
+    //determines the styling for the type button, based off of their type//
   }
 
-  function hideModal() {
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (x) => {
-    if (x.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  modalContainer.addEventListener('click', (x) => {
-    let target = x.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
   //end modal space//
 
   //function returns an object with keys associated to the above functions//
